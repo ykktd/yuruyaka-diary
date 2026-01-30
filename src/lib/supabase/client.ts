8777547f16686b567aeaa29b31e0f8eeb872b2cf
ykktd <129259907+ykktd@users.noreply.github.com>
2026-01-30 16:57:25 +0900

    chore: supabase用のclientとDB型定義を追加

---
+++ src/lib/supabase/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+import type { Database } from './database.types';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_PUBLISHABLE_KEY } from '$env/static/public';
+
+export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_PUBLISHABLE_KEY)
